52e0b557dfef673fb65e6d6fefbb6157e028b628
Mehdou MBULA <contact@boomrang-group.com>
2025-08-07 08:21:58 +0000

    Error: ./node_modules/@ffmpeg/ffmpeg/dist/esm/classes.js:105:28 [1m[31

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    // Exclure ffmpeg du rendu côté serveur
+    if (isServer) {
+      config.externals.push('@ffmpeg/ffmpeg', '@ffmpeg/util');
+    }
+    return config;
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
